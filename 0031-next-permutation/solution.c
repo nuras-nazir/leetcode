2663335ebb904190968589a82c28258d8579af42
nuras-nazir <142507209+nuras-nazir@users.noreply.github.com>
2026-02-09 16:43:25 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.8 MB (100.00%)

---
+++ 0031-next-permutation/solution.c
@@ -0,0 +1,32 @@
+void reverse(int* nums, int start, int end) {
+    while (start < end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void nextPermutation(int* nums, int numsSize) {
+    int i = numsSize - 2;
+
+    // Step 1: find pivot
+    while (i >= 0 && nums[i] >= nums[i + 1])
+        i--;
+
+    // Step 2: find just larger element and swap
+    if (i >= 0) {
+        int j = numsSize - 1;
+        while (nums[j] <= nums[i])
+            j--;
+
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    // Step 3: reverse suffix
+    reverse(nums, i + 1, numsSize - 1);
+}
+
