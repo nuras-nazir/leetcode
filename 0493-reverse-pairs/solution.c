c0135a113dc16646c1346633467be6ae794f191d
nuras-nazir <142507209+nuras-nazir@users.noreply.github.com>
2026-02-23 16:02:55 +0000

    Sync LeetCode submission Runtime - 210 ms (12.10%), Memory - 75.9 MB (33.12%)

---
+++ 0493-reverse-pairs/solution.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+
+int merge(int* nums, int left, int mid, int right) {
+    int count = 0;
+    int j = mid + 1;
+
+    // Count reverse pairs
+    for (int i = left; i <= mid; i++) {
+        while (j <= right && (long)nums[i] > 2L * nums[j]) {
+            j++;
+        }
+        count += (j - (mid + 1));
+    }
+
+    // Merge step
+    int* temp = (int*)malloc((right - left + 1) * sizeof(int));
+    int i = left, k = 0;
+    j = mid + 1;
+
+    while (i <= mid && j <= right) {
+        if (nums[i] <= nums[j])
+            temp[k++] = nums[i++];
+        else
+            temp[k++] = nums[j++];
+    }
+
+    while (i <= mid) temp[k++] = nums[i++];
+    while (j <= right) temp[k++] = nums[j++];
+
+    for (i = 0; i < k; i++)
+        nums[left + i] = temp[i];
+
+    free(temp);
+    return count;
+}
+
+int mergeSort(int* nums, int left, int right) {
+    if (left >= right) return 0;
+
+    int mid = left + (right - left) / 2;
+    int count = 0;
+
+    count += mergeSort(nums, left, mid);
+    count += mergeSort(nums, mid + 1, right);
+    count += merge(nums, left, mid, right);
+
+    return count;
+}
+
+int reversePairs(int* nums, int numsSize) {
+    return mergeSort(nums, 0, numsSize - 1);
+}
