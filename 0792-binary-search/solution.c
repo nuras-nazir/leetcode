160a0bf208d7cd997d345733fe537f8b1696be38
nuras-nazir <142507209+nuras-nazir@users.noreply.github.com>
2026-02-25 16:20:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.3 MB (100.00%)

---
+++ 0792-binary-search/solution.c
@@ -0,0 +1,16 @@
+int search(int* nums, int numsSize, int target) {
+    int i, mid , low, high;
+    low = 0;
+    high = numsSize - 1;
+    while(low <= high){
+        mid = low + (high - low) / 2;
+        if(target == nums[mid]){
+            return mid;
+        }
+        else if(target < nums[mid])
+        high = mid - 1;
+        else
+        low = mid + 1;
+    }
+    return -1;
+}
