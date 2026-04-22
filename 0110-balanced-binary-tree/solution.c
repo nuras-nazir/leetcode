3c0dcd86c416cf18468da78d755eddf6c226da3c
nuras-nazir <142507209+nuras-nazir@users.noreply.github.com>
2026-04-22 15:32:39 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.2 MB (45.00%)

---
+++ 0110-balanced-binary-tree/solution.c
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int checkHeight(struct TreeNode* root) {
+    if (root == NULL) return 0;
+
+    int left = checkHeight(root->left);
+    if (left == -1) return -1;
+
+    int right = checkHeight(root->right);
+    if (right == -1) return -1;
+
+    if (abs(left - right) > 1) return -1;
+
+    return (left > right ? left : right) + 1;
+}
+
+bool isBalanced(struct TreeNode* root) {
+    return checkHeight(root) != -1;
+}
