741fb34f5b9a0eb0a6e6626d4dbb09e81f0bfac4
nuras-nazir <142507209+nuras-nazir@users.noreply.github.com>
2026-05-02 13:01:25 +0000

    Sync LeetCode submission Runtime - 5 ms (81.33%), Memory - 23.3 MB (84.67%)

---
+++ 0789-kth-largest-element-in-a-stream/solution.c
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+
+typedef struct {
+    int* heap;
+    int size;
+    int k;
+} KthLargest;
+
+// swap
+void swap(int* a, int* b) {
+    int t = *a; *a = *b; *b = t;
+}
+
+// heapify up
+void heapifyUp(KthLargest* obj, int i) {
+    while (i > 0) {
+        int parent = (i - 1) / 2;
+        if (obj->heap[parent] <= obj->heap[i]) break;
+        swap(&obj->heap[parent], &obj->heap[i]);
+        i = parent;
+    }
+}
+
+// heapify down
+void heapifyDown(KthLargest* obj, int i) {
+    while (1) {
+        int left = 2*i + 1, right = 2*i + 2, smallest = i;
+
+        if (left < obj->size && obj->heap[left] < obj->heap[smallest])
+            smallest = left;
+        if (right < obj->size && obj->heap[right] < obj->heap[smallest])
+            smallest = right;
+
+        if (smallest == i) break;
+
+        swap(&obj->heap[i], &obj->heap[smallest]);
+        i = smallest;
+    }
+}
+
+KthLargest* kthLargestCreate(int k, int* nums, int numsSize) {
+    KthLargest* obj = (KthLargest*)malloc(sizeof(KthLargest));
+    obj->heap = (int*)malloc(sizeof(int) * k);
+    obj->size = 0;
+    obj->k = k;
+
+    for (int i = 0; i < numsSize; i++) {
+        // reuse add logic
+        if (obj->size < k) {
+            obj->heap[obj->size++] = nums[i];
+            heapifyUp(obj, obj->size - 1);
+        } else if (nums[i] > obj->heap[0]) {
+            obj->heap[0] = nums[i];
+            heapifyDown(obj, 0);
+        }
+    }
+
+    return obj;
+}
+
+int kthLargestAdd(KthLargest* obj, int val) {
+    if (obj->size < obj->k) {
+        obj->heap[obj->size++] = val;
+        heapifyUp(obj, obj->size - 1);
+    } else if (val > obj->heap[0]) {
+        obj->heap[0] = val;
+        heapifyDown(obj, 0);
+    }
+
+    return obj->heap[0];
+}
+
+void kthLargestFree(KthLargest* obj) {
+    free(obj->heap);
+    free(obj);
+}
