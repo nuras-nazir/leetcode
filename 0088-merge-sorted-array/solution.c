94c3c78ea9c40ed55f127b7e44fb71b37058b2ed
nuras-nazir <142507209+nuras-nazir@users.noreply.github.com>
2026-05-13 14:00:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.6 MB (94.51%)

--- 0088-merge-sorted-array/solution.c
+++ 0088-merge-sorted-array/solution.c
@@ -1,16 +1,29 @@
-void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
-     int i = m - 1;               // last valid element in nums1
-    int j = n - 1;               // last element in nums2
-    int k = nums1Size - 1;       // last index of nums1
+void merge(int* nums1, int nums1Size, int m,
+           int* nums2, int nums2Size, int n) {
 
-    while (i >= 0 && j >= 0) {
-        if (nums1[i] > nums2[j])
-            nums1[k--] = nums1[i--];
-        else
-            nums1[k--] = nums2[j--];
+    int i = m - 1;
+    int j = n - 1;
+    int k = m + n - 1;
+
+    while(i >= 0 && j >= 0) {
+
+        if(nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        }
+        else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+
+        k--;
     }
 
-    while (j >= 0) {
-        nums1[k--] = nums2[j--];
+    while(j >= 0) {
+
+        nums1[k] = nums2[j];
+
+        j--;
+        k--;
     }
 }
