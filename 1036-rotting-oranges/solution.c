e112c2fb1a52c9297af469501f0ccf76aa790c1c
nuras-nazir <142507209+nuras-nazir@users.noreply.github.com>
2026-05-31 14:41:39 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10 MB (63.01%)

--- 1036-rotting-oranges/solution.c
+++ 1036-rotting-oranges/solution.c
@@ -1,57 +1,72 @@
-#include <stdlib.h>
+typedef struct
+{
+    int r;
+    int c;
+} Queue;
 
-typedef struct {
-    int x, y;
-} Node;
-
-int orangesRotting(int** grid, int gridSize, int* gridColSize) {
+int orangesRotting(int** grid, int gridSize, int* gridColSize)
+{
     int rows = gridSize;
     int cols = gridColSize[0];
 
-    Node queue[10000];
-    int front = 0, rear = 0;
+    Queue q[10000];
+    int front = 0;
+    int rear = 0;
 
     int fresh = 0;
 
-    // Step 1: push all rotten
-    for(int i = 0; i < rows; i++) {
-        for(int j = 0; j < cols; j++) {
-            if(grid[i][j] == 2) {
-                queue[rear++] = (Node){i, j};
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < cols; j++)
+        {
+            if(grid[i][j] == 2)
+            {
+                q[rear++] = (Queue){i,j};
             }
-            else if(grid[i][j] == 1) {
+            else if(grid[i][j] == 1)
+            {
                 fresh++;
             }
         }
     }
 
-    if(fresh == 0) return 0;
+    if(fresh == 0)
+        return 0;
 
     int minutes = 0;
-    int dirs[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
 
-    // Step 2: BFS
-    while(front < rear) {
+    int dr[] = {-1,1,0,0};
+    int dc[] = {0,0,-1,1};
+
+    while(front < rear)
+    {
         int size = rear - front;
-        int infected = 0;
+        int changed = 0;
 
-        for(int i = 0; i < size; i++) {
-            Node curr = queue[front++];
+        for(int i = 0; i < size; i++)
+        {
+            Queue curr = q[front++];
 
-            for(int d = 0; d < 4; d++) {
-                int nx = curr.x + dirs[d][0];
-                int ny = curr.y + dirs[d][1];
+            for(int d = 0; d < 4; d++)
+            {
+                int nr = curr.r + dr[d];
+                int nc = curr.c + dc[d];
 
-                if(nx >= 0 && ny >= 0 && nx < rows && ny < cols && grid[nx][ny] == 1) {
-                    grid[nx][ny] = 2;
-                    queue[rear++] = (Node){nx, ny};
+                if(nr >= 0 && nr < rows &&
+                   nc >= 0 && nc < cols &&
+                   grid[nr][nc] == 1)
+                {
+                    grid[nr][nc] = 2;
                     fresh--;
-                    infected = 1;
+
+                    q[rear++] = (Queue){nr,nc};
+                    changed = 1;
                 }
             }
         }
 
-        if(infected) minutes++;
+        if(changed)
+            minutes++;
     }
 
     return (fresh == 0) ? minutes : -1;
