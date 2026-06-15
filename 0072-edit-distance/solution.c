a2ed7f8e7e3e1c753eeb632afe63c55d9b396540
nuras-nazir <142507209+nuras-nazir@users.noreply.github.com>
2026-06-15 14:38:57 +0000

    Sync LeetCode submission Runtime - 4 ms (47.96%), Memory - 10.4 MB (45.82%)

---
+++ 0072-edit-distance/solution.c
@@ -0,0 +1,33 @@
+int min(int a, int b)
+{
+    return (a < b) ? a : b;
+}
+
+int minDistance(char* word1, char* word2)
+{
+    int m = strlen(word1);
+    int n = strlen(word2);
+
+    int dp[m + 1][n + 1];
+
+    for(int i = 0; i <= m; i++)
+        dp[i][0] = i;
+
+    for(int j = 0; j <= n; j++)
+        dp[0][j] = j;
+
+    for(int i = 1; i <= m; i++)
+    {
+        for(int j = 1; j <= n; j++)
+        {
+            if(word1[i - 1] == word2[j - 1])
+                dp[i][j] = dp[i - 1][j - 1];
+            else
+                dp[i][j] = 1 + min(dp[i - 1][j - 1],
+                              min(dp[i - 1][j],
+                                  dp[i][j - 1]));
+        }
+    }
+
+    return dp[m][n];
+}
