132f525141591e156018c0bc00530b0e86ebdf69
nuras-nazir <142507209+nuras-nazir@users.noreply.github.com>
2026-06-16 15:32:31 +0000

    Sync LeetCode submission Runtime - 42 ms (58.28%), Memory - 13.1 MB (42.94%)

---
+++ 0516-longest-palindromic-subsequence/solution.c
@@ -0,0 +1,36 @@
+int max(int a, int b)
+{
+    return (a > b) ? a : b;
+}
+
+int longestPalindromeSubseq(char* s)
+{
+    int n = strlen(s);
+
+    int dp[n][n];
+
+    for(int i = 0; i < n; i++)
+        dp[i][i] = 1;
+
+    for(int len = 2; len <= n; len++)
+    {
+        for(int i = 0; i <= n - len; i++)
+        {
+            int j = i + len - 1;
+
+            if(s[i] == s[j])
+            {
+                if(len == 2)
+                    dp[i][j] = 2;
+                else
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+            }
+            else
+            {
+                dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[0][n - 1];
+}
