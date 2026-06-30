380490e3e2853e679f1280651bc1781508bb45f4
nuras-nazir <142507209+nuras-nazir@users.noreply.github.com>
2026-06-30 16:23:05 +0000

    Sync LeetCode submission Runtime - 62 ms (15.36%), Memory - 19.1 MB (48.14%)

--- 0217-contains-duplicate/solution.c
+++ 0217-contains-duplicate/solution.c
@@ -1,18 +1,14 @@
-#include <stdlib.h>
-#include <stdbool.h>
-
- int compare(const void* a, const void* b) {
-    return (*(int*)a - *(int*)b);
-} 
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
 
 bool containsDuplicate(int* nums, int numsSize) {
     qsort(nums, numsSize, sizeof(int), compare);
 
     for (int i = 1; i < numsSize; i++) {
-        if (nums[i] == nums[i - 1]) {
+        if (nums[i] == nums[i - 1])
             return true;
-        }
     }
+
     return false;
 }
-
