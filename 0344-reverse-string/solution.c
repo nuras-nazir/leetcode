41d943af47718ab6626a41cc25f93efb58d0664a
nuras-nazir <142507209+nuras-nazir@users.noreply.github.com>
2026-07-08 15:54:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.9 MB (12.95%)

--- 0344-reverse-string/solution.c
+++ 0344-reverse-string/solution.c
@@ -1,8 +1,10 @@
 void reverseString(char* s, int sSize) {
+
     int left = 0;
     int right = sSize - 1;
 
-    while (left < right) {
+    while(left < right){
+
         char temp = s[left];
         s[left] = s[right];
         s[right] = temp;
